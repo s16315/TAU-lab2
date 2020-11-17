35aa0279b1e7ee1f63bed5c75480bea2deb5402f
Tomasz Mnich <s16315@pjwstk.edu.pl>
2020-11-17 20:44:22 +0100

    refaktoryzacja klas

--- main.py
+++ main.py
@@ -1,35 +1,7 @@
-import time
+
 
 from selenium import webdriver
-
-
-class SpeedTestPl:
-    """
-    Klasa testująca działanie strony www.speedtest.pl
-    """
-    def __init__(self, driver):
-        self.driver = driver
-        self.driver.get('https://www.speedtest.pl/')
-
-    def rodo_click(self):
-        """
-        Metoda umożliwiająca przejście popup-u z rodo blokującego dalszą aktywność.
-        """
-        self.driver.find_element_by_id('rodoClick').click()
-
-    def main_menu_test(self):
-        step1 = self.driver.find_element_by_id('topmenu')
-        step2 = step1.find_elements_by_tag_name('li')
-        step2[2].click()
-
-    def end_test(self):
-        """
-        Metoda kończąca test.
-        :return:
-        """
-        time.sleep(4)
-        self.driver.close()
-
+from speedtestpl import SpeedTestPl
 
 speedTest = SpeedTestPl(webdriver.Chrome(executable_path='./Drivers/chromedriver.exe'))
 speedTest.rodo_click()
